d84a7672559e5500d74c89f61c79224668fbe68a
theo <theospielman@gmail.com>
2017-12-12 14:31:45 -0800

    Complete sticky header and smooth scrolling

--- app/assets/scripts/modules/StickyHeader.js
+++ app/assets/scripts/modules/StickyHeader.js
@@ -1,11 +1,20 @@
 import $ from 'jquery'
 import waypoints from '../../../../node_modules/waypoints/lib/noframework.waypoints'
+import smoothScroll from 'jquery-smooth-scroll'
 
 class StickyHeader {
   constructor() {
     this.siteHeader = $(".site-header")
     this.headerTriggerElement = $(".large-hero__title")
     this.createHeaderWaypoint()
+    this.pageSections = $(".page-section")
+    this.headerLinks = $(".primary-nav a")
+    this.createPageSectionWaypoints()
+    this.addSmoothScrolling()
+  }
+
+  addSmoothScrolling() {
+    this.headerLinks.smoothScroll()
   }
 
   createHeaderWaypoint() {
@@ -19,6 +28,36 @@ class StickyHeader {
         	that.siteHeader.removeClass("site-header--dark")
         }
       }
+    })
+  }
+
+  createPageSectionWaypoints() {
+    let that = this
+    this.pageSections.each(function() {
+      let currentPageSection = this
+      new Waypoint({
+        element: currentPageSection,
+        handler: function(direction) {
+          if (direction == "down") {
+            let matchingHeaderLink = currentPageSection.getAttribute("data-matching-link")
+            that.headerLinks.removeClass("is-current-link")
+            $(matchingHeaderLink).addClass("is-current-link")
+          }
+        },
+        offset: "18%"
+      })
+
+      new Waypoint({
+        element: currentPageSection,
+        handler: function(direction) {
+          if (direction == "up") {
+            let matchingHeaderLink = currentPageSection.getAttribute("data-matching-link")
+            that.headerLinks.removeClass("is-current-link")
+            $(matchingHeaderLink).addClass("is-current-link")
+          }
+        },
+        offset: "-40%"
+      })
     })
   }
 }
